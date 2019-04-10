d341f77e9b0f10745085921ac9d9c138cebb6ef0
Zhhenya <37300979+Zhhenya@users.noreply.github.com>
2019-04-10 19:10:18 +0700

    save

--- src/main/resources/program3.java
+++ src/main/resources/program3.java
@@ -1,23 +1,20 @@
 public class TestClass {
     int y = 9;
 
-    public class A {
-        int fibonacchi(int n) {
-            if (n == 0) {
-                return 0;
-            }
-            if (n == 1) {
-                return 1;
-            }
-            int result = fibonacchi(n - 1);
-            int rez = fibonacchi(n - 2);
-            int all = result + rez;
-            return all;
+    int fibonacchi(int n) {
+        if (n == 0) {
+            return 0;
         }
+        if (n == 1) {
+            return 1;
+        }
+        int result = fibonacchi(n - 1);
+        int rez = fibonacchi(n - 2);
+        int all = result + rez;
+        return all;
     }
 
     void main(int u) {
-        A.fibonacchi(8);
-        y = y + fibonacchi(6);
+        y = y + fibonacchi(8);
     }
 }#
